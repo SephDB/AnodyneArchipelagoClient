eef21d6b0ebb5c36edf255d50abb285fafbaaed3
Seph De Busser <seph.debusser@gmail.com>
2024-09-02 15:51:25 +0200

    Fix nexus pad spawning crash

--- AnodyneArchipelago/Plugin.cs
+++ AnodyneArchipelago/Plugin.cs
@@ -17,7 +17,7 @@ namespace AnodyneArchipelago
         public static Player Player => (Player)playerField.GetValue(Game.CurrentState as PlayState)!;
         public static ArchipelagoManager? ArchipelagoManager = null;
 
-        public const string Version = "0.2.4";
+        public const string Version = "0.2.5-hotfix";
 
         public void Load()
         {
