d592fe9471e03b630554c6ae07d35292b141c860
Seph De Busser <seph.debusser@gmail.com>
2025-02-06 15:42:22 +0100

    Fix #20, replace all card gates with our own that guards against null

---
+++ AnodyneArchipelago/Entities/FixedCardGate.cs
@@ -0,0 +1,24 @@
+﻿using AnodyneSharp.Dialogue;
+using AnodyneSharp.Entities;
+using AnodyneSharp.Entities.Gadget;
+using AnodyneSharp.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneArchipelago.Entities
+{
+    [NamedEntity("CardGate","")] //Set type to "" to have higher priority than the actual card gate
+    public class FixedCardGate(EntityPreset p, Player player) : BigCardGate(p,player)
+    {
+        public override bool TryUnlock()
+        {
+            bool ret = base.TryUnlock();
+
+            //Fix crash on enough cards + card count between 37 and 46
+            GlobalState.Dialogue ??= DialogueManager.GetDialogue("misc", "any", "keyblockgate", 4);
+
+            return ret;
+        }
+    }
+}
